502567f28369a3e4e17dcbcd07aaf1e2c17c2110
Anatoly.Nikitin <Anatoly.Nikitin@jetbrains.com>
2023-04-13 17:25:10 +0300

    Fix generateProto.sh

--- grpc/clients/typescript/generateProto.sh
+++ grpc/clients/typescript/generateProto.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 cd "$(dirname "$0")"
 mkdir -p src/generated
-export PATH="node_modules/protoc-gen-grpc-web/bin:node_modules/protoc/bin:$PATH"
+export PATH="node_modules/protoc-gen-grpc-web/bin:node_modules/protoc/protoc/bin:$PATH"
 protoc -I=../.. protocol/src/main/proto/service.proto --js_out=import_style=commonjs:src/generated --grpc-web_out=import_style=typescript,mode=grpcwebtext:src/generated
